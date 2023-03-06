7720cd36519218eb70de2d5488af45853516facf
Javier Pajares <javierpaor@gmail.com>
2023-03-06 02:45:06 +0100

    better popups and push notification on fall

--- static/js/common.js
+++ static/js/common.js
@@ -1,15 +1,70 @@
 const accelerationThreshold = 15;
+let isFallAlertActive = true;
 window.addEventListener(
   "devicemotion",
   (event) => {
     if (
-      Math.abs(event.acceleration.x) > accelerationThreshold ||
-      Math.abs(event.acceleration.y) > accelerationThreshold ||
-      Math.abs(event.acceleration.z) > accelerationThreshold
+      isFallAlertActive &&
+      (Math.abs(event.acceleration.x) > accelerationThreshold ||
+        Math.abs(event.acceleration.y) > accelerationThreshold ||
+        Math.abs(event.acceleration.z) > accelerationThreshold)
     ) {
-      alert(
-        `¡¿Te has caido?! (${event.acceleration.x}, ${event.acceleration.y}, ${event.acceleration.z})`
-      );
+      isFallAlertActive = false;
+      const popupHTML = `
+<div class="popup alert">
+  <div class="popup-header">
+    <h2>¡Caida Detectada!</h2>
+  </div>
+  <div class="popup-content">
+    <p>Hemos detectado una posible caida, y avisaremos a tus personas de contacto en <span class="countdown"></span> segundos.</p>
+  </div>
+  <hr>
+  <div class="popup-footer">
+    <button id="cancel-alert">Estoy bien, cancelar alerta</button>
+  </div>
+</div>
+        `;
+
+      const popupWrapper = document.createElement("div");
+      popupWrapper.classList.add("popup-wrapper");
+      popupWrapper.innerHTML = popupHTML;
+
+      document.querySelector("body").appendChild(popupWrapper);
+
+      const counter = popupWrapper.querySelector(".countdown");
+      let count = 20;
+      let isTicking = true;
+
+      function updateCounter() {
+        if (isTicking) {
+          if (count === 0) {
+            debugger;
+            fetch(
+              "https://backend.myseniorcare.es/notify-all-but-me",
+              //"http://localhost:3333/notify-all-but-me",
+              {
+                method: "GET",
+                credentials: "include",
+              }
+            ).then(() => {});
+            isTicking = false;
+            isFallAlertActive = true;
+          } else {
+            count--;
+            counter.innerHTML = count;
+            setTimeout(updateCounter, 1000);
+          }
+        }
+      }
+      updateCounter();
+
+      popupWrapper
+        .querySelector("#cancel-alert")
+        .addEventListener("click", () => {
+          isTicking = false;
+          isFallAlertActive = true;
+          popupWrapper.style.display = "none";
+        });
     }
   },
   false
@@ -98,30 +153,45 @@ async function registerServiceWorker() {
   setTimeout(function () {
     if (Notification.permission === "default") {
       const popupHTML = `
-          <div class="popup" style="position: absolute; top: 25%; left: 10%; right: 10%">
-            <h2>Grant Permission</h2>
-            <p>This site would like to send you push notifications. Click Allow to grant permission.</p>
-            <button id="allow-button">Allow</button>
-            <button id="deny-button">Deny</button>
-          </div>
+<div class="popup">
+  <div class="popup-header">
+    <h2>Activar Notificaciones</h2>
+  </div>
+  <div class="popup-content">
+    <p>Para recibir alertas que te ayuden a aprovechar mejor el cuidado que proporciona SeniorCare, activa las notificaciones.</p>
+  </div>
+  <hr>
+  <div class="popup-footer">
+    <button id="allow-button">Activar</button>
+    <button class="secondary" id="deny-button">Rechazar</button>
+  </div>
+</div>
         `;
 
-      const popupContainer = document.createElement("div");
-      popupContainer.innerHTML = popupHTML;
-      document.querySelector("body").appendChild(popupContainer);
+      const popupWrapper = document.createElement("div");
+      popupWrapper.classList.add("popup-wrapper");
+      popupWrapper.innerHTML = popupHTML;
 
-      popupContainer
+      document.querySelector("body").appendChild(popupWrapper);
+
+      popupWrapper
         .querySelector("#allow-button")
         .addEventListener("click", () => {
-          popupContainer.style.display = "none";
+          popupWrapper.style.display = "none";
           askForPermission().then(() => {});
         });
 
-      popupContainer
+      popupWrapper
         .querySelector("#deny-button")
         .addEventListener("click", () => {
-          popupContainer.style.display = "none";
+          popupWrapper.style.display = "none";
         });
+
+      popupWrapper.addEventListener("click", (event) => {
+        if (event.target === popupWrapper) {
+          popupWrapper.style.display = "none";
+        }
+      });
     }
   }, 5000);
 }
