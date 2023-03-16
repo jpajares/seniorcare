b09159226367777078bbcea3ad38f6f90e089054
Javier Pajares <javierpaor@gmail.com>
2023-03-16 22:01:31 +0100

    sos button

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,5 @@
 const accelerationThreshold = 15;
-const homeCoordinates = [36.739786061885276, -4.554898971493983] // lat, lon
+const homeCoordinates = [36.739786061885276, -4.554898971493983]; // lat, lon
 let currentCoordinates;
 
 let isFallAlertActive = true;
@@ -40,7 +40,6 @@ window.addEventListener(
       function updateCounter() {
         if (isTicking) {
           if (count === 0) {
-            debugger;
             fetch(
               "https://backend.myseniorcare.es/notify-all-but-me",
               //"http://localhost:3333/notify-all-but-me",
@@ -82,7 +81,7 @@ setTimeout(
 );
 */
 
-if (!document.cookie) {
+if (!document.cookie && crypto.randomUUID) {
   const uuid = crypto.randomUUID();
   const expiration = new Date(
     new Date().getTime() + 365 * 24 * 60 * 60 * 1000
@@ -155,9 +154,13 @@ async function registerServiceWorker() {
   async function askForGeolocationPermission() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        alert(`Estás a ${distance(position.coords.latitude, position.coords.longitude, ...homeCoordinates).toFixed(1)}km de tu casa`);
-        // https://maps.google.com/?q=<lat>,<lng>&z=<zoom>
-        // qq.src = `https://maps.google.com/maps?q=${lat},${lon}&z=17&output=embed`
+        alert(
+          `Estás a ${distance(
+            position.coords.latitude,
+            position.coords.longitude,
+            ...homeCoordinates
+          ).toFixed(1)}km de tu casa`
+        );
       },
       () => {
         // Handle permission denied or error
@@ -186,7 +189,7 @@ async function registerServiceWorker() {
         const popupHTML = `
         <div class="popup">
           <div class="popup-header">
-            <h2>Activar Notificaciones</h2>
+            <h2>Activar Permisos</h2>
           </div>
           <div class="popup-content">
             <p>Para habilitar un mejor el cuidado proporcionado por SeniorCare, activa ${pending
@@ -246,18 +249,137 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function distance(lat1, lon1, lat2, lon2) {
   const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2-lat1);
-  const dLon = deg2rad(lon2-lon1);
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
   const a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c; // Distance in km
   return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+  return deg * (Math.PI / 180);
 }
+
+let sosButtonFunctionality = "drag";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.querySelector("#sos_button");
+  let pressTimer;
+
+  myButton.addEventListener("mousedown", longPressStart);
+  myButton.addEventListener("touchstart", longPressStart);
+
+  myButton.addEventListener("mouseup", longPressEnd);
+  myButton.addEventListener("touchend", longPressEnd);
+
+  function longPressStart(event) {
+    if (sosButtonFunctionality === "longPress") {
+      event.preventDefault();
+      clearInterval(pressTimer);
+      pressTimer = window.setInterval(() => {
+        clearInterval(pressTimer);
+        fetch(
+          "https://backend.myseniorcare.es/notify-all-but-me",
+          //"http://localhost:3333/notify-all-but-me",
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        ).then(() => {});
+      }, 3000);
+    }
+  }
+
+  function longPressEnd(event) {
+    if (sosButtonFunctionality === "longPress") {
+      clearInterval(pressTimer);
+      sosButtonFunctionality = "disabled";
+      myButton.classList.remove("enabled");
+      setTimeout(() => (sosButtonFunctionality = "drag"), 100);
+    }
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const clickableElement = document.querySelector("#sos_button > a");
+  const movableElement = document.querySelector("#sos_button");
+
+  let draggingStartTime;
+  let initialX;
+  let initialY;
+  let currentX;
+  let currentY;
+  let xOffset = parseInt(localStorage.getItem("xOffset")) || 0;
+  let yOffset = parseInt(localStorage.getItem("yOffset")) || 0;
+  setTranslate(xOffset, yOffset, movableElement);
+
+  // Add event listeners for both mouse and touch events
+  clickableElement.addEventListener("mousedown", dragStart);
+  clickableElement.addEventListener("touchstart", dragStart);
+  clickableElement.addEventListener("mouseup", dragEnd);
+  clickableElement.addEventListener("touchend", dragEnd);
+  clickableElement.addEventListener("mousemove", drag);
+  clickableElement.addEventListener("touchmove", drag);
+
+  function dragStart(event) {
+    if (sosButtonFunctionality === "drag") {
+      event.preventDefault();
+      if (event.type === "touchstart") {
+        initialX = event.touches[0].clientX - xOffset;
+        initialY = event.touches[0].clientY - yOffset;
+      } else {
+        initialX = event.clientX - xOffset;
+        initialY = event.clientY - yOffset;
+      }
+
+      if (event.target === clickableElement) {
+        draggingStartTime = new Date();
+      }
+    }
+  }
+
+  function drag(event) {
+    if (sosButtonFunctionality === "drag") {
+      if (new Date() - draggingStartTime >= 200) {
+        event.preventDefault();
+        if (event.type === "touchmove") {
+          xOffset = event.touches[0].clientX - initialX;
+          yOffset = event.touches[0].clientY - initialY;
+        } else {
+          xOffset = event.clientX - initialX;
+          yOffset = event.clientY - initialY;
+        }
+
+        setTranslate(xOffset, yOffset, movableElement);
+        localStorage.setItem("xOffset", xOffset);
+        localStorage.setItem("yOffset", yOffset);
+      }
+    }
+  }
+
+  function dragEnd(event) {
+    if (sosButtonFunctionality === "drag") {
+      if (new Date() - draggingStartTime < 200) {
+        sosButtonFunctionality = "disabled";
+        movableElement.classList.add("enabled");
+        setTimeout(() => (sosButtonFunctionality = "longPress"), 100);
+      } else {
+        event.preventDefault();
+      }
+      initialX = currentX;
+      initialY = currentY;
+
+      draggingStartTime = null;
+    }
+  }
+
+  function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  }
+});
